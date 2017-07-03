1ffcf18b3b4288bc0a451a80be82e86c2c7c572d
swlee-PC\swlee <inter-lsw@hanmaill.net>
2017-07-04 02:12:16 +0900

    no message

---
+++ src/main/java/com/eeswk/EeswkApplication.java
@@ -0,0 +1,12 @@
+package com.eeswk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeswkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeswkApplication.class, args);
+	}
+}
